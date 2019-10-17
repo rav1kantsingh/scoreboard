0819836d9db37a337a7b993ccddc4e3cfd779337
XDivergent <ravikantsingh2308@gmail.com>
2019-10-17 06:16:53 +0530

    uploading data working

--- src/pages/Admin.jsx
+++ src/pages/Admin.jsx
@@ -4,36 +4,18 @@ import Leaderboard from '../component/Leaderboard';
 
 export default class MainPage extends React.Component {
 
-    // handleResponse = (data) => {
-    //     var responseData = {
-    //         ...data
-    //     };
-
-    //     // Get a key for a new Post.
-    //     var newPostKey = firebase.database().ref().child('posts').push().key;
-
-    //     // Write the new post's data simultaneously in the posts list and the user's post list.
-    //     var updates = {};
-    //     updates['/posts/' + newPostKey] = postData;
-    //     updates['/user-posts/' + uid + '/' + newPostKey] = postData;
-
-    //     return firebase.database().ref().update(updates);
-    // }
-
-render() {
-    return (
-        <div id='main-page'>
-            <div className='ranking'>
-                <Leaderboard />
-            </div>
-            <div className='main'>
-                <div className='card-container'>
-                    <ScoreCard />
-                    <ScoreCard />
-                    <ScoreCard />
+    render() {
+        return (
+            <div id='main-page'>
+                <div className='ranking'>
+                    <Leaderboard />
+                </div>
+                <div className='main'>
+                    <div className='card-container'>
+                        <ScoreCard editable='true' />
+                    </div>
                 </div>
             </div>
-        </div>
-    )
-}
+        )
+    }
 }
