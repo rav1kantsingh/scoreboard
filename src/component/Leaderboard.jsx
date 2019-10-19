a9808339dec21f06ac4b418b09d67e56ae96bf4c
arsundram <arsundram@gmail.com>
2019-10-19 20:18:10 +0530

    Main Page Leaderboard fixed -- firebase connected

--- src/component/Leaderboard.jsx
+++ src/component/Leaderboard.jsx
@@ -10,14 +10,18 @@ export default class Leaderboard extends React.Component {
     }
   }
 
-  componentDidMount(){
-    const dataRef = firebase.database().ref('ranking');
+  componentDidMount() {
+    var dataList = [];
+    const dataRef = firebase.database().ref('total-score');
     dataRef.on('value', snapshot => {
       const dataObject = snapshot.val();
-      const dataList = Object.keys(dataObject).map(key => ({
-        ...dataObject[key],
-      }));
-
+      console.log("aman", dataObject);
+      for (var key in dataObject) {
+        var singleeObj = {};
+        singleeObj['branch'] = key;
+        singleeObj['score'] = dataObject[key];
+        dataList.push(singleeObj);
+      }
       this.setState({
         data: dataList
       })
@@ -27,13 +31,13 @@ export default class Leaderboard extends React.Component {
   render() {
     this.state.data.sort((a, b) => b.score - a.score);
     return (
-      
+
       <div className="listbox">
-      <div id='title'>LEADERBOARD</div>
+        <div id='title'>LEADERBOARD</div>
 
         <ul>
-          {this.state.data.map((item,index) => {
-            return <div><LeaderboardListItem  rank={index+1} key={index+1} branch={item.branch} score={item.score} /></div>
+          {this.state.data.map((item, index) => {
+            return <div><LeaderboardListItem rank={index + 1} key={index + 1} branch={item.branch} score={item.score} /></div>
           })}
         </ul>
       </div>
