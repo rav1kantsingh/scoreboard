6111ffba22b2e4122e2e48e5c69047510cb12889
arsundram <arsundram@gmail.com>
2019-10-19 19:36:50 +0530

    firebase -admin panel fixed

--- src/pages/Admin.jsx
+++ src/pages/Admin.jsx
@@ -21,19 +21,22 @@ export default class MainPage extends React.Component {
     const dataRef = firebase.database().ref('performers');
     dataRef.on('value', snapshot => {
       const dataObject = snapshot.val();
+      // console.log("dataobject:",dataObject);
       const dataList = Object.keys(dataObject).map(key => ({
         ...dataObject[key],
       }));
+      // console.log("dataList:",dataList);
 
       this.setState({
         data: dataList
+
       })
     })
 
     //reading current total score of branch.
-    const totalScore = firebase.database().ref('/total-score'+ this.state.branch);
+    const totalScore = firebase.database().ref('/total-score');
     totalScore.on('value', snapshot => {
-      const dataObject = snapshot.child(this.state.branch).val();
+      const dataObject = snapshot.val();
       this.setState({
         read: dataObject
       })
@@ -43,14 +46,46 @@ export default class MainPage extends React.Component {
 
   handleSubmitClicked = (score) => {
     this.updateDB(score);
+    this.updateBRANCH(score);
+    this.updateEVENT(score);
   }
 
   updateDB = (score) => {
-    const key = firebase.database().ref().child('temp').push().key;
+   
     const updates = {};
+    updates[this.state.index+1+"/judge1"] = score;
     console.log(this.state.read)
-
-    return firebase.database().ref().update(updates);
+    return firebase.database().ref('/performers').update(updates);
+  }
+  updateBRANCH = (score) => {
+    var oldScore = 0;
+    firebase.database().ref('/total-score/'+this.state.branch).on('value', function(snapshot) {
+      // console.log("old",snapshot.val());
+      oldScore = (parseInt(snapshot.val()));
+  });
+   
+    //update branch score
+    const updates={};
+    updates["/"+this.state.branch] = score+oldScore;
+    // console.log(this.state.read)
+    return firebase.database().ref('/total-score').update(updates);
+  
+  
+  }
+  updateEVENT = (score) => {
+    var oldScore = 0;
+    firebase.database().ref('/'+this.state.event+'/'+this.state.branch).on('value', function(snapshot) {
+      console.log("old",snapshot.val());
+      oldScore = (parseInt(snapshot.val()));
+  });
+   
+    //update branch score
+    const updates={};
+    updates["/"+this.state.branch] = score+oldScore;
+    console.log(this.state.read)
+    return firebase.database().ref('/'+this.state.event).update(updates);
+  
+  
   }
 
   handleClick = (index) => {
