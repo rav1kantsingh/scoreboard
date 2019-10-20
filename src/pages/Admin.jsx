ad756512ef56c3e27e6b1308903c4b86e428e438
XDivergent <ravikantsingh2308@gmail.com>
2019-10-20 12:24:13 +0530

    uncomplete work

--- src/pages/Admin.jsx
+++ src/pages/Admin.jsx
@@ -12,8 +12,10 @@ export default class MainPage extends React.Component {
       event: '',
       data: [],
       index: '',
-      read:0,
-      judge_id: 'j1'
+      read: 0,
+      judge_id: 'j1',
+      isSelected: false,
+      isVisible: false,
     }
   }
 
@@ -29,7 +31,6 @@ export default class MainPage extends React.Component {
 
       this.setState({
         data: dataList
-
       })
     })
 
@@ -41,9 +42,13 @@ export default class MainPage extends React.Component {
         read: dataObject
       })
     })
-    
+
   }
   handleClick = (index) => {
+    this.setState({
+      isSelected: !this.state.isSelected,
+    })
+
     this.setState({
       name: this.state.data[index].name,
       branch: this.state.data[index].branch,
@@ -61,79 +66,69 @@ export default class MainPage extends React.Component {
   }
 
   updateDB = (score) => {
-   
+
     const updates = {};
-    updates[this.state.index+1+"/"+this.state.judge_id] = score;
+    updates[this.state.index + 1 + "/" + this.state.judge_id] = score;
     console.log(this.state.read)
     return firebase.database().ref('/performers/').update(updates);
   }
   updateBRANCH = (score) => {
- 
-   
+
+
     //update branch score
-    const updates={};
+    const updates = {};
     updates[this.state.judge_id] = score;
     console.log(this.state.read)
-    return firebase.database().ref('/'+this.state.event+'/'+this.state.branch+'/JUDGES').update(updates);
-  
- 
+    return firebase.database().ref('/' + this.state.event + '/' + this.state.branch + '/JUDGES').update(updates);
+
+
   }
   updateEVENT = (score) => {
-      
+
     //update event score
-    const updates={};
+    const updates = {};
     updates[this.state.judge_id] = score;
     console.log(this.state.read)
-    return firebase.database().ref('/'+this.state.event+'/'+"JUDGES").update(updates);
+    return firebase.database().ref('/' + this.state.event + '/' + "JUDGES").update(updates);
 
-  
+
   }
 
-updateLEADERBOARD = (score) => {
+  updateLEADERBOARD = (score) => {
     var oldScore = 0;
-    firebase.database().ref('/'+this.state.event+'/'+"leaderboard/"+'/'+this.state.branch).on('value', function(snapshot) {
-      console.log("old",snapshot.val());
+    firebase.database().ref('/' + this.state.event + '/' + "leaderboard/" + '/' + this.state.branch).on('value', function (snapshot) {
+      console.log("old", snapshot.val());
       oldScore = (parseInt(snapshot.val()));
-  });
+    });
 
-    const updates={};
-    updates[this.state.branch] = score+oldScore;
+    const updates = {};
+    updates[this.state.branch] = score + oldScore;
     console.log(this.state.read)
-    return firebase.database().ref('/'+this.state.event+'/'+"leaderboard/").update(updates);
-}
-//   updateJUDGE = (score) => {
+    return firebase.database().ref('/' + this.state.event + '/' + "leaderboard/").update(updates);
+  }
+  //   updateJUDGE = (score) => {
 
-   
-//     //update branch score
-//     const updates={};
-//     updates["/"+this.state.branch] = score;
-//     console.log(this.state.read)
-//     return firebase.database().ref('/'+this.state.event).update(updates);
-  
-  
-//   }
-//   handleClick = (index) => {
-//     this.setState({
-//       name: this.state.data[index].name,
-//       branch: this.state.data[index].branch,
-//       event: this.state.data[index].event,
-//       index: index
-//     })
-//   }
+
+  //     //update branch score
+  //     const updates={};
+  //     updates["/"+this.state.branch] = score;
+  //     console.log(this.state.read)
+  //     return firebase.database().ref('/'+this.state.event).update(updates);
 
   render() {
     return (
       <div className='admin-page'>
         <div className='ranking'>
-        <Contestants data={this.state.data} handleClick={this.handleClick} />
+          <Contestants data = {this.state.data} color={this.state.color}  handleClick={this.handleClick} />
         </div>
         <div className='main'>
-          <div className='card-container'>
+          {this.state.isVisible ?  <div className={'card-container'}>
             <h2 className='details'>{this.state.name}</h2>
             <h3 className='details'>{this.state.branch}</h3>
             <h3 className='details'>{this.state.event}</h3>
             <AdminScoreCard data={this.state.data} handleSubmitClicked={this.handleSubmitClicked} />
-          </div>
+          </div> : null}
+         
         </div>
       </div>
     )
